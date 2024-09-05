b47060d06c709b9f0eb33acc5e867576c75a9157
sivakumarsetti <sivakumarsettii@gmail.com>
2024-09-05 12:25:48 +0530

    form validation

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,21 +1,43 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import {appCtx} from '../context/appCtx'
 import { Input } from '@/common/components/Input'
 import config  from './config.json'
 
 export const Login = () => {
+    const[inputControls,setinputControls] = useState(config)
     const ctxData = useContext(appCtx)
     const fnLogin = () => {
-        ctxData.dispatch({
-            type: "LOGIN",
-            payload: true
+        const dataObj : any = {}
+        const clonedInputControls = JSON.parse(JSON.stringify(inputControls))
+        clonedInputControls.forEach((obj :any) =>{                 //check each input field have data or not
+            dataObj[obj.name] = obj.value
+            obj.hasError = !obj.value                            //if data is there then update hasError is false otherwise true
         })
+        const isInValid = clonedInputControls.some((obj:any)=> obj.hasError) //If any of the input object with hasError is true then it is invalid
+        if(isInValid){                                    //if it is invalid then form is updated using setinputControl()   
+            setinputControls(clonedInputControls)
+            return
+        }
+        alert(`Sending data to the server ${JSON.stringify(dataObj)}`)    //if valid form then send the data to server
+        // ctxData.dispatch({     
+        //     type: "LOGIN",
+        //     payload: true
+        // })
     }
+    const handleChange = (event:any) =>{
+          const{name,value} = event?.target
+          const clonedInputControls = JSON.parse(JSON.stringify(inputControls))
+          let inputObj :any = clonedInputControls.find((obj :any)=>obj.name === name)
+          inputObj.value = value
+          inputObj.hasError = !value
+          setinputControls(clonedInputControls)
+    }
+    // console.log(inputControls)
     return (
         <div className='container-fluid'>
             <h3 className='text-center mt-3 mb-3'>Login</h3>
-            {config.map(obj=>{
-                return <Input {...obj}/>
+            {inputControls.map(obj=>{
+                return <Input {...obj} key={obj.name} handleChange={handleChange}/>
             })}
             <div className='row'>
                 <div className='offset-sm-5 col-sm-7'>
--- src/common/components/Input/Input.tsx
+++ src/common/components/Input/Input.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
-export const Input = ({lbl,lblCols,inputCols,errorMsg,type,name,errMsgCols,handleChange}:any) => {
+export const Input = ({lbl,lblCols,inputCols,errorMsg,hasError,value,isRequired,type,name,errMsgCols,handleChange}:any) => {
   return (
     <div className='row mb-3'>
         <div className={`col-sm-${lblCols} text-end`}>
-            <b>{lbl}:</b>
+            <b>{lbl}{isRequired && <span className='text-danger'>*</span>}:</b>
         </div>
         <div className={`col-sm-${inputCols}`}>
-            <input type={type} className='form-control'/>
+            <input type={type} name={name} className='form-control' onChange={handleChange}/>
         </div>
         <div className={`col-sm-${errMsgCols}`}>
-            <b className='text-danger'>{errorMsg}</b>
+            {hasError && <b className='text-danger'>{errorMsg}</b>}
         </div>
     </div>
   )
 }
+
