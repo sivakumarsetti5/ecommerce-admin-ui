976d11348f4ed00e9857fe5c47bfc648a34b42e3
sivakumarsetti <sivakumarsettii@gmail.com>
2024-09-19 11:50:13 +0530

    latest

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import menuItems from './config.json'
+import Link from 'next/link'
+import styles from './Menu.module.css'
 
 export const Menu = () => {
+    const[isMobileView,setIsMobileView] = useState(document.body.offsetWidth<700)
+    function handleResize(){
+        window.addEventListener('resize',()=>{
+            setIsMobileView(document.body.offsetWidth<700)
+        })
+    }
+    useEffect(()=>{
+        handleResize()
+    },[])
     return (
-        <div>Menu</div>
+        <>
+        {isMobileView && <button>Menu</button>}
+        <ul className={styles.menu}>
+            {
+            menuItems?.map(({id,name,path})=>{
+                return <li><Link id={id} href={path}>{name}</Link></li>
+            })
+            }
+        </ul>
+        </>
     )
 }
